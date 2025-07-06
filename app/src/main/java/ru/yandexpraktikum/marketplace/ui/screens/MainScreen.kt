2563c173945185779a9ec347d69c4f1e3401f89c
artem1klas <baryshev964086@yandex.ru>
2025-07-07 00:26:34 +0300

    task_1

--- app/src/main/java/ru/yandexpraktikum/marketplace/ui/screens/MainScreen.kt
+++ app/src/main/java/ru/yandexpraktikum/marketplace/ui/screens/MainScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -192,7 +193,7 @@ fun ProductCard(
                         .clickable {
                             onAddToCart()
                         }
-                        .size(16.dp)
+                        .minimumInteractiveComponentSize()
                 )
             }
         }
